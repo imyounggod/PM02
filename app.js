26002a15744cd4aeeb31f35f114e6bfb489da93d
imyounggod <den.galyamin@mail.ru>
2020-06-23 03:01:10 +0300

    Обновлены серверная и клиентская часть

--- app.js
+++ app.js
@@ -1,50 +1,69 @@
 //Подключение модулей
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 //Объект приложения Express
 const app = express();
 
 //Установка движка представлений
 app.set("view engine", "hbs");
-
+//Обработка статических файлов
+app.use(express.static(__dirname + "/public"));
 //Парсер для данных
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-<<<<<<< HEAD
-
-
-const mysql = require('mysql2');
-
+//Объект движка представленией
+const hbs = require("hbs");
+//Частичные представления
+hbs.registerPartials(__dirname + "/views/partials");
+ 
+//Подключение к базе данных
 const connection = mysql.createConnection({
   host: "mansur228.beget.tech",
   user: "mansur228_228",
   database: "mansur228_228",
   password: "FZ8H%LDg",
-  port: "3306"
+  port: "3306",
+  dateStrings: 'date'
 });
-=======
-
-// тестирование подключения
-
 
 
 //Обработчики маршрутов
+
 app.get("/", function(req,res){
-  connection.query("SELECT aircraft.title_aircraft, airport.title_airport FROM flight, aircraft, airport", function(err, data) {
+  console.log("Переход на \"/\"")
+  res.render("index.hbs"), {
+  }
+});
+
+app.get("/plan", function(req,res){
+  connection.query(`SELECT DISTINCT flight.id_flight, 
+  (SELECT airport.title_airport FROM airport,flight WHERE airport.id_airport = flight.id_airsend) AS title_out,
+  (SELECT airport.city FROM airport, flight WHERE airport.id_airport = flight.id_airsend) AS city_out,
+  (SELECT airport.country FROM airport, flight WHERE airport.id_airport = flight.id_airsend) AS country_out,
+  (SELECT airport.title_airport FROM airport, flight WHERE airport.id_airport = flight.id_aircoming) AS title_in,
+  (SELECT airport.city FROM airport, flight WHERE airport.id_airport = flight.id_aircoming) AS city_in,
+  (SELECT airport.country FROM airport, flight WHERE airport.id_airport = flight.id_aircoming) AS country_in,
+  schedule.date_out, schedule.date_in 
+  FROM flight, airport,schedule`,
+    function(err, data) {
     if(err) return console.log(err);
-    res.render("index.hbs", {
-        flight: data
+    res.render("plan.hbs", {
+      schedule: data
     });
+    
   });
 });
 
 app.get("/about", function(req,res){
-    res.send("<h1>About</h1>");
+    res.render("about.hbs");
 });
 
 app.get("/contact", function(req,res){
-    res.send("<h1>Contact</h1>");
+    res.render("contact.hbs");
 });
-
+app.get("/public*", function (req, res) {
+  res.redirect("/about");
+});
 //Прослушивание сервера
 app.listen(3000);
 
---
+++ public/js/elements_custom.js
@@ -0,0 +1,417 @@
+/* JS Document */
+
+/******************************
+
+[Оглавление]
+
+1. Переменные и единицы
+2. Установка заголовка
+3. Начальное меню
+4. Добавление Progress Bars
+5. Добавление Accordions
+6. Инициализация этапов
+7. Начальные загрузчики
+8. Начальная форма поиска
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Переменные и единицы
+
+	*/
+
+	var menu = $('.menu');
+	var menuActive = false;
+	var header = $('.header');
+	var ctrl = new ScrollMagic.Controller();
+	var searchActive = false;
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initProgressBars();
+	initAccordions();
+	initMilestones();
+	initLoaders();
+	initSearchForm();
+
+	/* 
+
+	2. Установка заголовка
+
+	*/
+
+	function setHeader()
+	{
+		if(window.innerWidth < 992)
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		else
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		if(window.innerWidth > 991 && menuActive)
+		{
+			closeMenu();
+		}
+	}
+
+	/* 
+
+	3. Начальное меню
+
+	*/
+
+	function initMenu()
+	{
+		if($('.hamburger').length && $('.menu').length)
+		{
+			var hamb = $('.hamburger');
+			var close = $('.menu_close_container');
+
+			hamb.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+
+			close.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+	/* 
+
+	4. Добавление Progress Bars
+
+	*/
+
+	function initProgressBars()
+	{
+		if($('.skill_bars').length)
+		{
+			var bars = $('.skill_bars');
+
+			bars.each(function()
+			{
+				var ele = $(this);
+	    		var elePerc = ele.data('perc');
+	    		var eleName = '#' + ele.attr('id');
+	    		var color = "#fa9e1b";
+	    		if(ele.attr('data-color'))
+	    		{
+	    			color = ele.attr('data-color');
+	    		}
+	    		var statsScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var pbar = new ProgressBar.Line(eleName, 
+		    		{
+		    			strokeWidth: 1,
+						easing: 'easeInOut',
+						duration: 1400,
+						color: color,
+						trailColor: '#e1e1e1',
+						trailWidth: 1,
+						svgStyle: {display: 'block', width: '100%', height: '100%'},
+						text: {
+							style: {
+								// Цвет текста
+								// По умолчанию: такой же, как цвет обводки (options.color)
+								fontFamily: 'Open Sans',
+								textAlign: 'right',
+								fontSize: '14px',
+								width: '40px',
+								color: '#1a1a1a',
+								position: 'absolute',
+								left: 'calc' + '(' + (elePerc * 100) + '% - 40px' + ')',
+								top: '-39px',
+								padding: 0,
+								margin: 0,
+								transform: null
+								},
+								autoStyleContainer: false
+						},
+						step: function(state, bar) {
+						bar.setText('+' + Math.round(bar.value() * 100) + ' %');
+						}
+		    		});
+		    		pbar.animate(elePerc);
+		    	})
+		    	.addTo(ctrl);
+			})
+		}
+	}
+
+	/* 
+
+	5. Добавление Accordions
+
+	*/
+
+	function initAccordions()
+	{
+		if($('.accordion').length)
+		{
+			var accs = $('.accordion');
+
+			accs.each(function()
+			{
+				var acc = $(this);
+
+				if(acc.hasClass('active'))
+				{
+					var panel = $(acc.next());
+					var panelH = panel.prop('scrollHeight') + "px";
+					
+					if(panel.css('max-height') == "0px")
+					{
+						panel.css('max-height', panel.prop('scrollHeight') + "px");
+					}
+					else
+					{
+						panel.css('max-height', "0px");
+					} 
+				}
+
+				acc.on('click', function()
+				{
+					if(acc.hasClass('active'))
+					{
+						acc.removeClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						} 
+					}
+					else
+					{
+						acc.addClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						} 
+					}
+				});
+			});
+		}
+	}
+
+	/* 
+
+	6. Инициализация этапов
+
+	*/
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Используйте data-sign-before и data-sign-after,
+	    		 чтобы добавить знаки перед или за номером счетчика */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
+	/* 
+
+	7. Начальные загрузчики
+
+	*/
+
+	function initLoaders()
+	{
+		if($('.loader').length)
+		{
+			var loaders = $('.circle');
+
+			loaders.each(function()
+			{
+				var loader = $(this);
+				var endValue = $(loader).data('perc');
+
+				var loaderScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		loader.circleProgress({
+						startAngle: 4.66,
+						thickness: 3,
+						size: 176,
+						emptyFill:"transparent",
+						fill: {gradient:["#fa9e1b", "#8d4fff"]},
+					}).on('circle-animation-progress', function(event, progress, stepValue)
+						{
+							$(this).find('strong').text(Math.round(stepValue * 100) + "%");
+						});
+		    	})
+			    .addTo(ctrl);
+			});
+		}
+	}
+
+	/* 
+
+	8. Начальная форма поиска
+
+	*/
+
+	function initSearchForm()
+	{
+		if($('.search_form').length)
+		{
+			var searchForm = $('.search_form');
+			var searchInput = $('.search_content_input');
+			var searchButton = $('.content_search');
+
+			searchButton.on('click', function(event)
+			{
+				event.stopPropagation();
+
+				if(!searchActive)
+				{
+					searchForm.addClass('active');
+					searchActive = true;
+
+					$(document).one('click', function closeForm(e)
+					{
+						if($(e.target).hasClass('search_content_input'))
+						{
+							$(document).one('click', closeForm);
+						}
+						else
+						{
+							searchForm.removeClass('active');
+							searchActive = false;
+						}
+					});
+				}
+				else
+				{
+					searchForm.removeClass('active');
+					searchActive = false;
+				}
+			});	
+		}
+	}
+});
