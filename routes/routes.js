9c2932e5be32ac44a17521d72bc493fd857968e8
imyounggod <den.galyamin@mail.ru>
2020-07-02 02:10:12 +0300

    Обновление серверной части. Оформление заказа

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const moment = require('moment');
+
 module.exports = function(app,db){
+  let order = {};
   app.get("/", function(req,res){
     console.log("Переход на \"/\"")
     if(req.query.where && req.query.date_out){
@@ -37,13 +39,49 @@ module.exports = function(app,db){
         
       });
   });
-
-  app.post("/search", function (req, res) {
-         
-    if(!req.body) return res.sendStatus(400);
-    res.send(`req.body.fio=${req.body.fio}\nreq.body.flight=${req.body.flight}`)
-  });
   
+  app.post("/ticket", function (req,res) {
+    if(!req.body) return res.sendStatus(400);
+    res.render("ticket.hbs",{
+      data: req.body
+    })    
+  })
+
+  app.post("/ticket/confirm",function (req,res) {
+    if(!req.body) return res.sendStatus(400);
+    if(req.body.bagg){
+
+    }
+    let query = `INSERT INTO baggage (small_baggage, big_baggage) VALUES (`;
+    if(req.body.bagg){
+      query+= `"True", `;
+    }
+    else{
+      query+= `"False", `;
+    }
+    if(req.body.bagg2){
+      query+= `"True")`;
+    }
+    else{
+      query+= `"False")`;
+    }
+    db.query(query,function(err, data) {
+      if(err) return console.log(err);})
+    db.query("INSERT INTO client (`fio`,`series/number`,`id_b`) VALUES (?,?,(SELECT id_baggage FROM baggage ORDER BY id_baggage DESC LIMIT 1))",[String(req.body.fio),req.body.passport],function(err, data) {
+      if(err) return console.log(err);})
+    db.query("INSERT INTO ticket (`code_passenger`,`fio_passenger`,`number_fligt`,`number_place`,`row_num`,`class`,`time_ricket`) VALUES ((SELECT id_client FROM client ORDER BY id_client DESC LIMIT 1),?,?,?,?,?,?)",
+    [req.body.fio,req.body.flight,req.body.place,req.body.row,req.body.class, moment(new Date()).format("YYYY-MM-DD")],function(err, data) {
+      if(err) return console.log(err);})
+    db.query("INSERT INTO orders (`id_fli`,`number_row`,`number_place`,`date`,`id_c`,`classmode`,`id_t`) VALUES (?,?,?,?,(SELECT id_client FROM client ORDER BY id_client DESC LIMIT 1),?,(SELECT id_ticket FROM ticket ORDER BY id_ticket DESC LIMIT 1))",
+    [req.body.flight,req.body.row,req.body.place,req.body.date,req.body.class],function(err, data) {
+      if(err) return console.log(err);})
+    res.redirect("/");
+  });
+
+  app.get("/ticket/cancel",function (req,res) {
+    res.redirect("/search");
+  });
+
   app.get("/table", function(req,res){
     let where = req.query.where;
     let date_out = new Date(req.query.date_out);
