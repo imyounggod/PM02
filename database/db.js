ce80a5cd8e836f6e6a42901eb00447dcb3a42ac5
Galyamin <63673861+imyounggod@users.noreply.github.com>
2020-07-03 13:11:41 +0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,8 +3,7 @@ const mysql = require("mysql2");
 // Подключение файла с данными для подключения к бд
 const dbConfig = require("../config/db.config.js");
 
-// создаем соединение с нашей базой данных
-const connection = mysql.createConnection({
+
 
 // Создаем соединение с нашей базой данных
 const connection = mysql.createPool({
@@ -17,14 +16,5 @@ const connection = mysql.createPool({
     dateStrings: dbConfig.dateStrings
 });
 
-//Проверка соединения
-connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
 // Экспортируем  соединение
 module.exports = connection;
